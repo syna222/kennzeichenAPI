6c4554d81c78cd6aa27aba37590d8a25c6efe647
Viola Kaemmer <v.kaemmer@gmx.de>
2023-02-23 12:05:24 +0100

    added query options to main route.

--- controllers/Kennzeichen.js
+++ controllers/Kennzeichen.js
@@ -1,15 +1,54 @@
 const Kennzeichen = require("../models/Kennzeichen");
 
+function compareAlphab(a, b){
+    if(a.Kennzeichen < b.Kennzeichen){
+      return -1;
+    }
+    if(a.Kennzeichen > b.Kennzeichen){
+      return 1;
+    }
+    return 0;
+}
+
+function compareBL(a, b){
+    if(a.Bundesland < b.Bundesland){
+      return -1;
+    }
+    if(a.Bundesland > b.Bundesland){
+      return 1;
+    }
+    return 0;
+}
 
 const getAllKennzeichen = async (req, res) => {
+    const { sortkfz } = req.query;      // query a la "?sortkfz=true"
+    const { sortbu } = req.query;       //query a la "?sortbu=true"
     try{
         const kennzeichen = await Kennzeichen.find();
+        console.log("test aus getAllKennzeichen(), sortkfz ist:", sortkfz);
+        console.log("test aus getAllKennzeichen(), sortbu ist:", sortbu);
+        //OPT 1: sort alphabetically if sortkfz==="true":
+        if(sortkfz==="true"){
+            //console.log("sortkfz is string true");
+            kennzeichen.sort(compareAlphab);
+        }
+        //OPT 2: sort by bundesland if sortbu==="true":
+        if(sortbu==="true"){
+            //console.log("sortbu is string true");
+            kennzeichen.sort(compareBL);
+        }
+
         res.status(200).json(kennzeichen);
     }
     catch(err){
         console.log(err.message);
     }
 }
+
+
+
+
+
 
 const getSingleKennzeichen = async (req, res) => {
     const { kz } = req.params;
